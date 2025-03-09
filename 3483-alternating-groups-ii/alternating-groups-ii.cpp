0ed14154e5824630ee2300735b585a924cfaae77
NATANSH KAPIL <76418368+NatanshSep2@users.noreply.github.com>
2025-03-09 08:11:12 +0530

    Time: 61 ms (89.33%) | Memory: 102.6 MB (91.84%) - LeetSync

---
+++ 3483-alternating-groups-ii/alternating-groups-ii.cpp
@@ -0,0 +1,27 @@
+//NATANSH KAPIL
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        //maxlen is the window length while travising the array window lenght will be increase
+        // ans : no. of such windows
+        int maxLen = 1,ans = 0, n = colors.size();
+        //traversing the array uptill n-1+k-1 
+        //i = 1 becz we also have to check previous index
+        for(int i = 1; i <= n-1+k-1; i++)
+        {
+            //checking the same element its prev. and next element too
+            if(colors[i%n] != colors[(i-1+n)%n]){
+                //if it is not = than we increase the window size
+                maxLen++;
+            }else{
+                //but if it is equal than reset the window size
+                maxLen = 1;
+            }
+            //if my window size is morethan = to k and i inc. my ans
+            if(maxLen >= k)
+                ans++;
+        }
+    //retrun ans 
+        return ans;
+    }
+};
